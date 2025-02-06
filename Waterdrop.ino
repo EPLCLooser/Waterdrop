38b3872c8ad3c7a352688a0a9e14faee6c4821c1
EPLCLooser <lucas@norrflod.se>
2025-02-06 14:46:54 +0100

    kommentarer + optimering

--- Waterdrop.ino
+++ Waterdrop.ino
@@ -1,3 +1,10 @@
+/*
+* Fil: Waterdrop.ino
+* Creator: Lucas Norrflod
+* Date: 6/2-2025
+* This code simulate water in a 2d space with an accelerometer and displays it on an oled screen.
+*/
+
 //included libraries
 #include "U8glib.h"
 #include "Wire.h"
@@ -6,23 +13,18 @@
 int ADXL345 = 0x53;  // The ADXL345 sensor I2C address
 
 float X_out, Y_out, Z_out;  // Outputs
-const int numofdrops = 25;
-int oldestVal = 1;
-float xincline;
-float yincline;
-float drops[numofdrops][6]; // [vilken droppe][x-position; y-position; x-acceleration; y-acceleration; x-velocity; y-velocity]
+const int numofdrops = 10; //The number of drops that will appear on screen
+float xincline; //incline recieved from ADXL345 sensor on the x axis
+float yincline; //incline recieved from ADXL345 sensor on the y axis
+float drops[numofdrops][6]; // [Which drop][x-position; y-position; x-acceleration; y-acceleration; x-velocity; y-velocity]
 
-const float maxAcc = 50;
+const float maxAcc = 50; //The highest acceleration that the waterdrops can recieve. An acceleration of 100 means increasing the velocity by 1.
 
-
-
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
-
-//variables
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK); //instantiating the object
 
 void setup() {
-  Serial.begin(9600);  // Initiate serial communication for printing the results on the Serial monitor
-  Wire.begin();        // Initiate the Wire library
+  Wire.begin();  // Initiates the Wire library
+
   // Set ADXL345 in measuring mode
   Wire.beginTransmission(ADXL345);  // Start communicating with the device
   Wire.write(0x2D);                 // Access/ talk to POWER_CTL Register - 0x2D
@@ -30,11 +32,11 @@ void setup() {
   Wire.write(8);  // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable
   Wire.endTransmission();
   delay(10);
+  //This loop creates the starting position for each drop.
   for (int x = 0; x < numofdrops; x++) {
     drops[x][0] = x;
   }
   u8g.nextPage();
-  Serial.println(drops[0][0]);
 }
 
 void loop() {
@@ -50,20 +52,18 @@ void loop() {
   Z_out = (Wire.read() | Wire.read() << 8);  // Z-axis value
   Z_out = Z_out / 256;
 
-  //Lägger in accelerometerns värden i en array
-  xincline = X_out;
-  yincline = Y_out;
+  
+  xincline = X_out; //Assigns the output from the accelerometer's x-axis
+  yincline = Y_out; //Assigns the output from the accelerometer's y-axis
 
-  //mapar om medelvärdet av lutningens värde från accelerometern till acceleration i antal pixlar/sekund
+  //This for-loop 
   for (int i = 0; i < numofdrops; i++) {
-    drops[i][2] = map(xincline * 100, -93, 107, -maxAcc, maxAcc);
+    drops[i][2] = map(xincline * 100, -93, 107, -maxAcc, maxAcc);   //Maps the incline value and assigns it to the acceration
     drops[i][3] = map(yincline * 100, -110, 90, maxAcc, -maxAcc);
 
     //Räknar ut pixel hastighet
     drops[i][4] += drops[i][2] / 100;
     drops[i][5] += drops[i][3] / 100;
-    Serial.println(drops[0][2]);
-    Serial.println(drops[0][3]);
 
     //friktion för vattnet
     drops[i][4] = friction(drops[i][4]);
@@ -73,26 +73,26 @@ void loop() {
     drops[i][0] += drops[i][4];
     drops[i][1] += drops[i][5];
 
+    //All these if-statements check if the posiyion of any drop is outside screen.
     if (drops[i][0] < 0) {
-      drops[i][0] = 0;
-      drops[i][2] = 0;
-      drops[i][4] = 0;
+      drops[i][0] = 0; //sets the x-position to 0
+      drops[i][2] = 0; //sets the x-velocity to 0
+      drops[i][4] = 0; //sets the x-acceleration to 0
     } else if (drops[i][0] > 123) {
-      drops[i][0] = 123;
-      drops[i][2] = 0;
-      drops[i][4] = 0;
+      drops[i][0] = 123; //sets the x-position to 123
+      drops[i][2] = 0; //sets the x-velocity to 0
+      drops[i][4] = 0; //sets the x-acceleration to 0
     }
     if (drops[i][1] < 0) {
-      drops[i][1] = 0;
-      drops[i][3] = 0;
-      drops[i][5] = 0;
+      drops[i][1] = 0; //sets the y-position to 0
+      drops[i][3] = 0; //sets the x-velocity to 0
+      drops[i][5] = 0; //sets the x-acceleration to 0
     } else if (drops[i][1] > 63) {
-      drops[i][1] = 63;
-      drops[i][3] = 0;
-      drops[i][5] = 0;
+      drops[i][1] = 63; //sets the y-position to 63
+      drops[i][3] = 0; //sets the x-velocity to 0
+      drops[i][5] = 0; //sets the x-acceleration to 0
     }
-
-    
+    delay(1);
   }
 
   // kollar om någon droppe har samma position som någon annan och ändrar positionen
@@ -123,20 +123,25 @@ float friction(float Vel) {
   return Vel;
 }
 
-int samepos(float arr[numofdrops][6]) {
+
+/*
+*
+*Parameter: An array with [Which drop][x-position; y-position; x-acceleration; y-acceleration; x-velocity; y-velocity]
+*/
+int samepos(float arr[numofdrops][6]) {s
   for (int i = 0; i < numofdrops; i++) {
-    for (int u = 0; u < numofdrops; u += 1) {
-      if (arr[u][0] == arr[i][0] && u != i) {
+    for (int u = i+1; u < numofdrops; u++) {
+      if (arr[u][0] == arr[i][0]) {
         if (arr[u][1] == arr[i][1]) {
-          if (arr[u][0] == 123) {
-            if (arr[u][1] <= 30) {
+          if (arr[u][0] >= 60) { //The if-statement looks if the drop is in the right or left half of the screen
+            if (arr[u][1] <= 30) { //The if-statement looks if the drop is in the upper or lower half of the screen
               arr[u][1] += 1;
             } else {
               arr[u][1] -= 1;
             }
             arr[u][0] -= 1;
           } else {
-            if (arr[u][1] <= 30) {
+            if (arr[u][1] <= 30) { //The if-statement looks if the drop is in the upper or lower half of the screen
               arr[u][1] += 1;
             } else {
               arr[u][1] -= 1;
